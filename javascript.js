8c9652f3c4a4cc295c3efc002b5bbabc1feebdda
Alexander Joel <alexanderj146@gmail.com>
2022-10-02 16:33:45 -0300

    Primera Clase

--- javascript.js
+++ javascript.js
@@ -1,2 +1,16 @@
 //Segunda forma de escribir JavaScript - Buena Practica
-console.log("Bienvenidos a la primera clase de JavaScript")
+console.log("Bienvenidos a la primera clase de JavaScript") 
+
+//Formas de guardar espacios en memoria
+//Primer forma es var 
+
+//Declaramos  variable - Opción Mala Práctica
+var autor
+//Declarar y asignar valor, se llama "Inicializar"
+var titulo = "Primer Clase"
+console.log(autor)//undefined
+console.log(titulo)
+
+//Declaramos Variable - Buena Práctica 
+let editorial 
+console.log(editorial)
