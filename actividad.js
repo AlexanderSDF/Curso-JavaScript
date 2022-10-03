3131d8630a91871c5e3e733ceb81e55287e5fbca
Alexander Joel <alexanderj146@gmail.com>
2022-10-03 00:12:53 -0300

    tarea 1

--- actividad.js
+++ actividad.js
@@ -1,3 +1,63 @@
-let numero1 = parseInt(prompt("Ingrese el Primer número"))
-let numero2 = parseInt(prompt("Ingrese el segundo número")) 
-let resultadoSuma = numero1 + numero2
+//TAREA 1 CALCULADORA DE OPERACIONES BÁSICAS
+//Operaciones / Suma-Resta-Multiplicación-Division-Porcentaje
+alert("Usuario, para realizar una operación se le solicitara dos números y un operador ");
+let numero1 = parseInt(prompt("Indique el Número 1"));
+let operador = prompt("Indique la Operación: ");
+let numero2 = parseInt(prompt("Indique el Número 2"));
+function operaciones(numero1, numero2, operador) {
+  if (operador == "+") {
+    alert(numero1 + numero2);
+  }
+  if (operador == "-") {
+    alert(numero1 - numero2);
+  }
+  if (operador == "*") {
+    alert(numero1 * numero2);
+  }
+  if (operador == "/") {
+    alert(numero1 / numero2);
+  }
+  if (operador == "%") {
+    alert(numero1 * (numero2 / 100));
+  }
+}
+operaciones(numero1, numero2, operador);//termina el function
+/*
+//----------------------------------------------------------------------------------------------
+
+//TAREA 2 
+//Jugador con cantidad de partidos, goles y Promedio de goles por partido
+let jugador = (prompt("Indique el jugador: "));
+let partidos = parseInt(prompt("Indique el cantidad de partidos: "));
+let goles = parseInt(prompt("Indique el cantidad de goles del jugador: "));
+let alago = "Terrible jugador parece el Diegote"
+let burla = "Flojo el muchacho derecho al Paddle"
+
+let promedio = goles / partidos
+alert(jugador + " tiene un promedio de "  + promedio + " goles por partido.")
+if (promedio>=0.5) {
+  alert(alago)
+}
+if(promedio<0.5){
+  alert(burla)
+}
+
+
+//TAREA 3--------------------------------------------------------------------------------------------
+//Ingresar 3 notas sacar un promedio
+let alumno = (prompt("Indique su nombre: "));
+let nota1 = parseInt(prompt("Indique la nota del 1er parcial: "));
+let nota2 = parseInt(prompt("Indique la nota del 2do parcial: "));
+let nota3 = parseInt(prompt("Indique la nota del 3er parcial: "));
+let aprobado = "Felicitaciones usted cursara el Final tipo 'A' del modulo 4,5,6 la Fecha 15/11/22."
+let desaprobado = "Usted cursara el Final tipo 'B' del modulo 1,2,3,4,5,6 la Fecha 15/11/22."
+let promedio = (nota1 + nota2 + nota3) /3
+
+alert(alumno + " su promedio general de calificaciones es "  + promedio +"." )
+if (promedio>=6) {
+  alert(aprobado)
+}
+if(promedio<6){
+  alert(desaprobado)
+}
+*/
--- clase1.js
+++ clase1.js
@@ -79,11 +79,11 @@ console.log(num1-cadena1)
 
 //prompt -- cartel emergente de entrada
 //guarda sus datos default como string
-let numero1 = prompt("Ingrese el Primer número")
-let numero2 = prompt("Ingrese el segundo número")
 
 /*Usuario ingrese dos numero, por separado y esos dos números 
 se sumen en una tercer variable mediante un cartel emergente la suma*/
-let resultadoSuma = numero1 + numero2
-console.log(resultadoSuma)
-alert("El resultado de la suma es: "+resultadoSuma)
+let numero1 = parseInt(prompt("Ingrese el Primer número"));
+let numero2 = parseInt(prompt("Ingrese el segundo número"));
+let resultadoSuma = numero1 + numero2;
+console.log(resultadoSuma);
+alert("El resultado de la suma es: " + resultadoSuma);
