67e296ffff1e235c80fc665a2698974bd4dce3da
Alexander Joel <alexanderj146@gmail.com>
2022-10-02 21:46:04 -0300

    Curso de JavaScript

--- actividad.js
+++ actividad.js
@@ -1,9 +1,5 @@
 let numero1 = parseInt(prompt("Ingrese el Primer número"));
-let numero2 = prompt("Ingrese el segundo número");
+let numero2 = parseInt(prompt("Ingrese el segundo número")); 
 
 let resultadoSuma = numero1 + numero2;
-console.log(resultadoSuma);
 alert("El resultado de la suma es: " + resultadoSuma);
-
-
-let suma20 = 300 + 400
