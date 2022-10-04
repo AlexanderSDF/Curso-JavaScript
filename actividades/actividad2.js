0412ee4176f6233394762036b760eaef9f442dbf
Alexander <alexanderj146@gmail.com>
2022-10-04 17:16:07 -0300

    Actividad de Clase 2

--- actividades/actividad2.js
+++ actividades/actividad2.js
@@ -1,3 +1,4 @@
+ 
 //!Ejercicio clase 2:
 
 //!Usuario ingresa tres notas del 1 al 10 y sacamos el total y el promedio de esas notas
@@ -22,7 +23,7 @@ else {
   alert(alumno + " su promedio general de calificaciones es "  + promedioNota +"."+" Usted desaprobó la materia, deberá cursar el Final tipo 'B' del modulo 1, 2, 3, 4, 5 y 6 la Fecha 15/11/22.");
 }
 
-//!TAREA 2--------------------------------------------------------------------
+//!EJERCICIO 2--------------------------------------------------------------------
 //!Usuario ingresa color  favorito y se le devuelve si es un color Primario, Secundario o Terciario
 
 let colorFavorito = prompt("Ingrese su color Favorito.")
@@ -35,4 +36,21 @@ else if((colorFavorito.toLowerCase() == "violeta")||(colorFavorito.toLowerCase()
 }
 else{
   alert("El color "+ colorFavorito + " es un color terciario.")
+}
+
+
+//!EJERCICIO 3--------------------------------------------------------------------
+//!Usuario ingresa nombre de jugador, cantidad de partidos jugados  y goles anotados.
+//!se le devolverá al usuario el promedio de goles por partido
+let jugador = (prompt("Indique el jugador: "));
+let partidos = parseInt(prompt("Indique el cantidad de partidos: "));
+let goles = parseInt(prompt("Indique el cantidad de goles del jugador: "));
+
+let promedioGol = goles / partidos
+alert(jugador + " tiene un promedio de "  + promedioGol + " goles por partido.")
+if (promedioGol>=0.5) {
+  alert("Terrible jugador parece el Diegote")
+}
+else{
+  alert("Flojo el muchacho derecho al Paddle")
 }
